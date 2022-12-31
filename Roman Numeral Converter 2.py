1003e82b0741739957b4d727393ab2b1e92c3ce7
bradenacurtis801 <bradenacurtis801@gmail.com>
2022-12-30 23:51:25 -0700

    complete

--- Roman Numeral Converter 2.py
+++ Roman Numeral Converter 2.py
@@ -1,27 +1,30 @@
 def roman_decimal(input):
+    input = input.upper()
     decimalVal = 0
     tempDict = {
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000
-	}
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
 
     for index, val in enumerate(input):
         try:
-		if val not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
-    			raise ValueError
-		elif tempDict[input[index+1]] > tempDict[val] and [tempDict[input[index+1]] > tempDict[val] for val in input[index+2:]]:
-			decimalVal += tempDict[input[index+1]] - tempDict[input[index]]
-		elif tempDict[input[index-1]] > tempDict[input[index]]:
-			decimalVal += tempDict[index]
+            if val not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
+                raise ValueError
+            elif index < len(input) -1 and tempDict[input[index+1]] > tempDict[val]:
+                if all([tempDict[input[index+1]] > tempDict[val] for val in input[index+2:]]) and all([tempDict[input[index + 1]] <= tempDict[val] for val in input[:index]]):
+                    decimalVal += tempDict[input[index+1]] - tempDict[input[index]]
+                else:
+                    raise ValueError
+            elif index == 0 or tempDict[input[index - 1]] >= tempDict[val]:
+                decimalVal += tempDict[val]
         except IndexError:
-      
-            
+            continue
+    return decimalVal
 
 
-
-roman_decimal('CMI')			
+print(roman_decimal('xcix'))
