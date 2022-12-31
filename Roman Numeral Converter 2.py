399c966e5e5d53902ade89dec302d04711215e0b
bradenacurtis801 <97850331+bradenacurtis801@users.noreply.github.com>
2022-12-30 22:40:43 -0700

    Update Roman Numeral Converter 2.py

--- Roman Numeral Converter 2.py
+++ Roman Numeral Converter 2.py
@@ -1,7 +1,7 @@
 def roman_decimal(input):
-	decimalVal = 0
-	tempDict = {
-		'I': 1,
+    decimalVal = 0
+    tempDict = {
+		'I': 1, 
 		'V': 5,
 		'X': 10,
 		'L': 50,
@@ -9,9 +9,20 @@ def roman_decimal(input):
 		'D': 500,
 		'M': 1000
 	}
-	
-	for index, val in enumerate(input):
-		if val not in ['I','V','X','L','C','D','M']:
-			raise ValueError
-		elif tempDict[input[index+1]] > val and [tempDict[input[index+1]] > val for val in tempDict[input[index+2:-1]]]
-			
+    
+    for index, val in enumerate(input):
+        if val not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
+            raise ValueError
+        elif tempDict[input[index+1]] > tempDict[val] and [tempDict[input[index+1]] > tempDict[val] for val in input[index+2:]]:
+            decimalVal += tempDict[input[index+1]] - tempDict[input[index]]    
+        elif tempDict[input[index-1]] > tempDict[input[index]]:
+            decimalVal += tempDict[index]
+            
+
+      
+            
+def main():
+	roman_decimal('CMI')			
+
+if __name__ == "__main__":
+    	pass
